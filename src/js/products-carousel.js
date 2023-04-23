0123ac4f93a280e5afff2895045ac59eef32b301
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-04-23 03:25:27 -0300

    Modificando products-carousel.js para que possa ser usado em qualquerstatus

--- src/js/products-carousel.js
+++ src/js/products-carousel.js
@@ -1,73 +1,50 @@
-const productsList = document.querySelector(".products-list");
-function addListItem() {
-  productsList.innerHTML += (`
-    <li class="products-list_items" title="O combo contém produtos que entregam fios saudáveis e livres de pontas duplas por meio de ativos poderosos.">
-      <img class="product-image" src="./src/img/products/siage_nutri_rose_shamp_cond.jpg" alt="Combo nutri rose com shampoo e condicionador" draggable="false" />
-      <h3 class="product-title">Combo Nutri Rose: Shampoo 400ml + Condicionador 400ml</h3>
-      <p class="product-value-discount">R$ 127,98</p>
-      <p class="product-value">R$ 112,98</p>
-      <p class="product-value-installment">7x de R$ 16,14</p>
-      <button class="button-put-bag">+ Adicionar na sacola</button>
-    </li>
-  `);
-}
-
-for(let i = 0; i < 10; i++) {
-  addListItem();
-}
-
-// -----------------------------------
-
+const productsCarousel = document.querySelector(".products-carousel");
+const firstProduct = productsCarousel.querySelectorAll(".products-carousel_items")[0];
+const arrowIcons = document.querySelectorAll(".products-wrapper i");
 let isDragStart = false, prevPageX, prevScrollLeft;
-
-function dragStart(e) {
-  isDragStart = true;
-  prevPageX = e.pageX || e.touches[0].pageX;
-  prevScrollLeft = productsList.scrollLeft;
-}
-
-function dragging(e) {
-  if(!isDragStart) return;
-  e.preventDefault();
-  productsList.classList.add("dragging");
-  let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-  productsList.scrollLeft = prevScrollLeft - positionDiff;
-  showHideIcons();
-}
-
-function dragStop() {
-  isDragStart = false;
-  productsList.classList.remove("dragging");
-}
-
-productsList.addEventListener("mousedown", dragStart);
-productsList.addEventListener("touchstart", dragStart);
-
-productsList.addEventListener("mousemove", dragging);
-productsList.addEventListener("touchmove", dragging);
-
-productsList.addEventListener("mouseup", dragStop);
-productsList.addEventListener("mouseleave", dragStop);
-productsList.addEventListener("touchend", dragStop);
-
-// -----------------------
-
-const firstProduct = productsList.querySelectorAll(".products-list_items")[0];
-const arrowIcons = document.querySelectorAll(".products-content i");
-
 
 function showHideIcons() {
   // showing and hiding prev/next icon according to carousel scroll left value  
-  let scrollWidth = productsList.scrollWidth - productsList.clientWidth; // getting max scrollable width
-  arrowIcons[0].style.display = productsList.scrollLeft == 0 ? "none": "inline-block";
-  arrowIcons[1].style.display = productsList.scrollLeft == scrollWidth ? "none": "inline-block";
+  let scrollWidth = productsCarousel.scrollWidth - productsCarousel.clientWidth; // getting max scrollable width
+  arrowIcons[0].style.display = productsCarousel.scrollLeft == 0 ? "none": "inline-block";
+  arrowIcons[1].style.display = productsCarousel.scrollLeft == scrollWidth ? "none": "inline-block";
 }
 
 arrowIcons.forEach(icon => {
   icon.addEventListener("click", () => {
     let firstProductWidth = firstProduct.clientWidth + 14; // getting first list item width & adding 14 margin value
     // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-    productsList.scrollLeft += icon.id == "arrow-left" ? -firstProductWidth : firstProductWidth;
+    productsCarousel.scrollLeft += icon.id == "arrow-left" ? -(firstProductWidth * 2) : (firstProductWidth * 2);
     setTimeout(showHideIcons, 60); // calling function after 60ms;
   });
 });
+
+function dragStart(e) {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = productsCarousel.scrollLeft;
+}
+
+function dragging(e) {
+  if(!isDragStart) return;
+  e.preventDefault();
+  productsCarousel.classList.add("dragging");
+  let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  productsCarousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+}
+
+function dragStop() {
+  isDragStart = false;
+  productsCarousel.classList.remove("dragging");
+}
+
+productsCarousel.addEventListener("mousedown", dragStart);
+productsCarousel.addEventListener("touchstart", dragStart);
+
+productsCarousel.addEventListener("mousemove", dragging);
+productsCarousel.addEventListener("touchmove", dragging);
+
+productsCarousel.addEventListener("mouseup", dragStop);
+productsCarousel.addEventListener("mouseleave", dragStop);
+productsCarousel.addEventListener("touchend", dragStop);
---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const productsList = document.querySelector(".products-list");
+function addListItem() {
+  productsList.innerHTML += (`
+    <li class="products-list_items products-carousel_items" title="O combo contém produtos que entregam fios saudáveis e livres de pontas duplas por meio de ativos poderosos.">
+      <img class="product-image" src="./src/img/products/siage_nutri_rose_shamp_cond.jpg" alt="Combo nutri rose com shampoo e condicionador" draggable="false" />
+      <h3 class="product-title">Combo Nutri Rose: Shampoo 400ml + Condicionador 400ml</h3>
+      <p class="product-value-discount">R$ 127,98</p>
+      <p class="product-value">R$ 112,98</p>
+      <p class="product-value-installment">7x de R$ 16,14</p>
+      <button class="button-put-bag">+ Adicionar na sacola</button>
+    </li>
+  `);
+}
+
+for(let i = 0; i < 10; i++) {
+  addListItem();
+}
