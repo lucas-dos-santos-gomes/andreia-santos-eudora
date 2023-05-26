1cf70f02935769d08540bc919838bc8d070d7f84
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-25 21:33:53 -0300

    Corrigindo erro dos produtos

--- src/js/products-carousel.js
+++ src/js/products-carousel.js
@@ -1,3 +1,23 @@
+const productsList = document.querySelector(".products-list");
+function addListItem() {
+  productsList.innerHTML += (`
+    <li class="products-list_items" title="O combo contém produtos que entregam fios saudáveis e livres de pontas duplas por meio de ativos poderosos.">
+      <img class="product-image" src="./src/img/products/siage_nutri_rose_shamp_cond.jpg" alt="Combo nutri rose com shampoo e condicionador" draggable="false" />
+      <h3 class="product-title">Combo Nutri Rose: Shampoo 400ml + Condicionador 400ml</h3>
+      <p class="product-value-discount">R$ 127,98</p>
+      <p class="product-value">R$ 112,98</p>
+      <p class="product-value-installment">7x de R$ 16,14</p>
+      <button class="button-put-bag">+ Adicionar na sacola</button>
+    </li>
+  `);
+  console.log(productsList);
+}
+for (let i = 0; i < 20; i++) {
+  addListItem();
+}
+
+/* TESTE ACIMA */
+
 const productsCarouselAll = document.querySelectorAll(".products-carousel");
 const arrowIcons = document.querySelectorAll(".products-wrapper i");
 let isDragStart = false, prevPageX, prevScrollLeft;
