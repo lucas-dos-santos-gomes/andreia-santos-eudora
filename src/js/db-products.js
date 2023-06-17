c09dd2b527244f8a14daa9f8d7ca1d7a12ffe81c
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-17 20:34:52 -0300

    Finalizando terceiro item do db products

--- src/js/db-products.js
+++ src/js/db-products.js
@@ -23,12 +23,12 @@ export const tb_siageProducts = [
   },
   {
     id: id++,
-    description: "O combo contém produtos que entregam fios saudáveis e livres de pontas duplas por meio de ativos poderosos.",
-    imgSrc: "./src/img/products/siage_nutri_rose_shamp_cond.jpg",
-    imgAlt: "Combo Nutri Rose",
-    title: "Combo Nutri Rose: Shampoo 400ml + Condicionador 400ml",
-    discount: 127.98,
-    value: 112.98,
-    installments: 7
+    description: "Com ingredientes poderosos, o combo traz 3 produtos que recupera o cabelo dos danos com a terapia capilar.",
+    imgSrc: "./src/img/products/combo-siage-cica-therapy_shampoo_condicionador_mascara_.jpg",
+    imgAlt: "Combo Siage Cica Therapy",
+    title: "Combo Cica-Therapy: Shampoo 250ml + Condicionador 200ml + Máscara Capilar 250g",
+    discount: 0,
+    value: 147.97,
+    installments: 9
   }
 ];
