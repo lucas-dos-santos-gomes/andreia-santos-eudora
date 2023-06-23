69cd757426a233aaee210b545bed3703b589a697
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-23 00:20:55 -0300

    Finalizando estrutura do menu hamburguer

--- src/js/header-scroll.js
+++ src/js/header-scroll.js
@@ -1,5 +1,5 @@
 export const header = document.querySelector(".header");
-const linkFocused = document.querySelector("#focused");
+const linkFocused = document.querySelector(".focused");
 
 window.addEventListener('scroll', () => {
   header.classList.toggle("active", window.scrollY > 1);
