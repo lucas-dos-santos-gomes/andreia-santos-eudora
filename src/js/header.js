52c3cf5ec7dcbfce3ddb2cabd558481b27b84193
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-23 02:01:02 -0300

    Menu hambúrguer funcional

--- src/js/header.js
+++ src/js/header.js
@@ -5,3 +5,14 @@ window.addEventListener('scroll', () => {
   header.classList.toggle("active", window.scrollY > 1);
   linkFocused.classList.toggle("active", window.scrollY > 1);
 });
+
+const MENU_HAMBURGUER_CHECK = document.querySelector("#x-menu");
+const HAMBURGUER_NAV = document.querySelector(".hamburguer-nav");
+
+MENU_HAMBURGUER_CHECK.onclick = (e) => {
+  if(e.srcElement.checked) {
+    HAMBURGUER_NAV.style.transform = "translateY(0)";
+  } else {
+    HAMBURGUER_NAV.style.transform = "translateY(-100vh)";
+  }
+}
