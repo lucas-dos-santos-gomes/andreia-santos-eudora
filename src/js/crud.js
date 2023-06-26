5d5720d8483bc4ef7446455e78a329aeb9e14699
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-26 03:22:16 -0300

    Finalizando últimos bugs do localStorage

--- src/js/crud.js
+++ src/js/crud.js
@@ -1,7 +1,11 @@
-
 let bd_localStorage = []; // console.table(JSON.parse(localStorage.tb_bag));
 
 export function createLocalStorage(id) {
+  if(typeof localStorage.tb_bag != "undefined") {
+    if(JSON.parse(localStorage.tb_bag).length > bd_localStorage) {
+      bd_localStorage = bd_localStorage.concat(JSON.parse(localStorage.tb_bag));
+    }
+  }
   bd_localStorage.push({id: id, qtd: 1});
   localStorage.setItem("tb_bag", JSON.stringify(bd_localStorage));
 }
@@ -18,6 +22,9 @@ export function readLocalStorage(addBag, tables, list, btn) {
 }
 
 export function updateLocalStorage(id, qtd) {
+  if(bd_localStorage.length == 0) {
+    bd_localStorage = bd_localStorage.concat(JSON.parse(localStorage.tb_bag));
+  }
   let bdIdTest = bd_localStorage.filter(e => e.id == id);
   bd_localStorage[bd_localStorage.indexOf(bdIdTest[0])].qtd = qtd;
   localStorage.setItem("tb_bag", JSON.stringify(bd_localStorage));
